3ad832a1c0219fa9762b34da0b08323baf798015
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-01-27 12:49:19 +0400

    Improve docstring for from_file function
    Updated docstring for from_file function to clarify arguments and return type.

--- pipeline/pandas/2-from_file.py
+++ pipeline/pandas/2-from_file.py
@@ -3,14 +3,16 @@
 
 import pandas as pd
 
-df = pd.read_csv("filename.csv")
 
 def from_file(filename, delimiter):
     """
-    function from_file
+    Load data from a file into a pandas DataFrame.
+
     Args:
-        filename: file to load from
-        delimiter: column separator
-    Returns: loaded pd.DataFrame
+        filename (str): path to the file
+        delimiter (str): column separator
+
+    Returns:
+        pd.DataFrame: loaded data
     """
     return pd.read_csv(filename, sep=delimiter)
