8d75873d167d9eaa5adb1302b75615b44cf5e88b
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-01-27 18:10:48 +0400

    Create prune function to remove NaN Close values
    Refactor pruning logic into a function.

--- pipeline/pandas/8-prune.py
+++ pipeline/pandas/8-prune.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
+"""Prune rows with NaN Close values"""
 
-import pandas as pd
-from_file = __import__('2-from_file').from_file
 
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-
-df.dropna(subset=['Close'], inplace=True)
-
-print(df.head())
+def prune(df):
+    """Removes rows where Close is NaN"""
+    return df.dropna(subset=["Close"])
