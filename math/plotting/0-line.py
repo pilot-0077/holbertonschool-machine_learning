baad0d35201adac55859bf80e830ee376d44875a
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-02-02 15:33:07 +0400

    Refine docstring for line plotting function
    Updated docstring to clarify the plot details.

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
+"""
+0-line.py
+Plot y as a red line graph.
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
-    """Plot y as a solid red line with x-axis ranging from 0 to 10."""
-    y = np.arange(0, 11) ** 3
-    plt.figure(figsize=(6.4, 4.8))
+    """
+    Plot y = x^3 as a solid red line.
 
+    The x-axis ranges from 0 to 10.
+    """
+    y = np.arange(0, 11) ** 3
     x = np.arange(0, 11)
+
+    plt.figure(figsize=(6.4, 4.8))
     plt.plot(x, y, "r-")
     plt.xlim(0, 10)
     plt.show()
