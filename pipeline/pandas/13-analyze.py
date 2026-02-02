50d3c01277054a1a79c965cdc8a679dd0b977488
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-02-02 11:40:35 +0400

    Refactor analyze function for descriptive statistics
    Refactor analyze function to compute statistics for input DataFrame and exclude Timestamp column.

--- pipeline/pandas/13-analyze.py
+++ pipeline/pandas/13-analyze.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
+"""Compute descriptive statistics for all columns except Timestamp."""
 
-import pandas as pd
-from_file = __import__('2-from_file').from_file
 
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+def analyze(df):
+    """
+    Compute descriptive statistics for all columns except Timestamp.
 
-stats = df.iloc[:, 1:8].describe()
+    Args:
+        df (pd.DataFrame): input dataframe
 
-print(stats)
+    Returns:
+        pd.DataFrame: statistics dataframe
+    """
+    if "Timestamp" in df.columns:
+        df = df.drop(columns=["Timestamp"])
+    return df.describe()
