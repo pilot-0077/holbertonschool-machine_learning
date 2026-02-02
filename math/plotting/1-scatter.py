38e52a91964ff6ea8781951e35404c664522b742
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-02-02 15:35:25 +0400

    Implement scatter plot for height vs weight
    Complete the scatter plot function to visualize height vs weight with appropriate labels and styling.

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -1,16 +1,38 @@
+1. Scatter
+Complete the following source code to plot x ↦ y as a scatter plot:
+
+The x-axis should be labeled Height (in)
+The y-axis should be labeled Weight (lbs)
+The title should be Men's Height vs Weight
+The data should be plotted as magenta points
+hbt-ml@Holberton-ML:~$ cat 1-scatter.py
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
 
-mean = [69, 0]
-cov = [[15, 8], [8, 15]]
-np.random.seed(5)
-x, y = np.random.multivariate_normal(mean, cov, 2000).T
-y += 180
+def scatter():
 
-# your code here
-plt.xlabel('Height (in)')
-plt.ylabel('Weight (lbs)')
-plt.title("Men's Height vs Weight")
-plt.scatter(x, y, c='magenta')
-plt.show()
+    mean = [69, 0]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x, y = np.random.multivariate_normal(mean, cov, 2000).T
+    y += 180
+    plt.figure(figsize=(6.4, 4.8))
+
+    # your code here
+Result
+
+hbt-ml@Holberton-ML:~$ cat 1-main.py
+#!/usr/bin/env python3
+
+scatter = __import__('1-scatter').scatter
+
+scatter()
+hbt-ml@Holberton-ML:~$ ./1-main.py
+Example plot
+
+Repo:
+
+GitHub repository: holbertonschool-machine_learning
+Directory: math/plotting
+File: 1-scatter.py
