c4b9aa1e40f7a33bdfe548578a3ca89c93b9fe16
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-02-02 15:31:08 +0400

    Refactor plotting code into a line function
    Encapsulated plotting logic in a function and added x-axis data.

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -2,9 +2,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-y = np.arange(0, 11) ** 3
 
+def line():
+    """Plot y as a solid red line with x-axis ranging from 0 to 10."""
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
 
-plt.xlim(0, 10)
-plt.plot(y, color='red')
-plt.show()
+    x = np.arange(0, 11)
+    plt.plot(x, y, "r-")
+    plt.xlim(0, 10)
+    plt.show()
