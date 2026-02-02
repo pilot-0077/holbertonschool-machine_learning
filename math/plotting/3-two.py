6604f41e67994221136fffbfb56fda32130df525
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-02-02 15:44:28 +0400

    Refactor plotting code into a function
    Refactored code into a function 'two()' to encapsulate plotting logic and added comments for clarity.

--- math/plotting/3-two.py
+++ math/plotting/3-two.py
@@ -1,21 +1,35 @@
 #!/usr/bin/env python3
+"""
+3-two.py
+Plot two exponential decays on the same axes.
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(0, 21000, 1000)
-r = np.log(0.5)
-t1 = 5730
-t2 = 1600
-y1 = np.exp((r / t1) * x)
-y2 = np.exp((r / t2) * x)
 
-# your code here
-plt.xlabel('Time (years)')
-plt.ylabel('Fraction Remaining')
-plt.title("Exponential Decay of Radioactive Elements")
-plt.xlim(0, 20000)
-plt.ylim(0, 1)
-plt.plot(x, y1, c='red', linestyle='dashed', label='C-14')
-plt.plot(x, y2, c='green', label='Ra-226')
-plt.legend()
-plt.show()
+def two():
+    """
+    Plot C-14 and Ra-226 exponential decay curves with specified styles.
+
+    - C-14: dashed red line
+    - Ra-226: solid green line
+    """
+    x = np.arange(0, 21000, 1000)
+    r = np.log(0.5)
+    t1 = 5730
+    t2 = 1600
+    y1 = np.exp((r / t1) * x)
+    y2 = np.exp((r / t2) * x)
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.plot(x, y1, "r--", label="C-14")
+    plt.plot(x, y2, "g-", label="Ra-226")
+
+    plt.xlabel("Time (years)")
+    plt.ylabel("Fraction Remaining")
+    plt.title("Exponential Decay of Radioactive Elements")
+    plt.xlim(0, 20000)
+    plt.ylim(0, 1)
+    plt.legend(loc="upper right")
+    plt.show()
