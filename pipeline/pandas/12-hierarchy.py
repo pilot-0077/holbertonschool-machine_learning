0fede33888e6cf8311aa0ed525c86cea3fe84013
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-02-02 11:37:21 +0400

    Implement hierarchy function for dataframe concatenation
    Added a function to concatenate dataframes with a specific timestamp range and rearranged MultiIndex.

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
+"""Concatenate two tables with Timestamp as first MultiIndex level."""
 
 import pandas as pd
-from_file = __import__('2-from_file').from_file
 
-df1 = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-df2 = from_file('bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv', ',')
+index = __import__('10-index').index
 
-df1 = df1.set_index('Timestamp')
-df2 = df2.set_index('Timestamp')
-df = pd.concat([df2.loc["1417411980":"1417417980"],
-                df1.loc["1417411980":"1417417980"]],
-               keys=["bitstamp", "coinbase"])
-df = df.swaplevel(0, 1, axis=0)
-df = df.sort_index()
-print(df)
+
+def hierarchy(df1, df2):
+    """
+    Concatenate bitstamp and coinbase between timestamps 1417411980 and
+    1417417980 inclusive, then rearrange MultiIndex so Timestamp is first.
+    """
+    df1 = index(df1)
+    df2 = index(df2)
+
+    start = 1417411980
+    end = 1417417980
+
+    df1 = df1.loc[start:end]
+    df2 = df2.loc[start:end]
+
+    df = pd.concat([df2, df1], keys=["bitstamp", "coinbase"])
+
+    df = df.swaplevel(0, 1, axis=0)
+    df = df.sort_index()
+
+    return df
