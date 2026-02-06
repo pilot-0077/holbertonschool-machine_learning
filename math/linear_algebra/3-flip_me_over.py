6e720a916b41092a484d7451867a79f5a24786eb
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-02-06 12:58:19 +0400

    Update 3-flip_me_over.py

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,20 +1,9 @@
 #!/usr/bin/env python3
-""" Transpose function to check a new matrix transposed"""
+"""Transpose a 2D matrix."""
 
 
 def matrix_transpose(matrix):
-    """ return a new matrix transposed
-
-    Args:
-        matrix: given list
-
-    Return:
-        new_matrix: Transposed matrix
-
-    """
-    if type(matrix[0]) != list:
-        return [len(matrix)]
-    else:
-        new_matrix = [[matrix[j][i] for j in range(len(matrix))]
-                      for i in range(len(matrix[0]))]
-    return new_matrix
+    """Return the transpose of a 2D matrix."""
+    rows = len(matrix)
+    cols = len(matrix[0])
+    return [[matrix[r][c] for r in range(rows)] for c in range(cols)]
