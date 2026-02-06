38001ae8b812f70eadbc87c1013ae8349fa85423
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-02-06 16:18:04 +0400

    Update 8-ridin_bareback.py

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,27 +1,20 @@
 #!/usr/bin/env python3
-"""function to multiply 2 matrices"""
+"""Matrix multiplication."""
 
 
 def mat_mul(mat1, mat2):
-    """ multiply two matrices
-
-    Args:
-        mat1, mat2: Given matrices
-
-    Return:
-        the new mat: new_mat
-    """
+    """Perform matrix multiplication (mat1 x mat2)."""
     if len(mat1[0]) != len(mat2):
         return None
-    else:
-        new_mat = []
-        for i in range(len(mat1)):
-            mat_i = []
-            for j in range(len(mat2[0])):
-                vec = 0
-                for k in range(len(mat2)):
-                    vec += mat1[i][k] * mat2[k][j]
-                mat_i.append(vec)
-            new_mat.append(mat_i)
-        for x in new_mat:
-            return new_mat
+
+    result = []
+    for i in range(len(mat1)):
+        row = []
+        for j in range(len(mat2[0])):
+            s = 0
+            for k in range(len(mat2)):
+                s += mat1[i][k] * mat2[k][j]
+            row.append(s)
+        result.append(row)
+
+    return result
