be02ff7af25dff8649247efdefc13ab516eb1ca7
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-02-06 19:52:46 +0400

    Update 14-saddle_up.py

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,19 +1,9 @@
 #!/usr/bin/env python3
-"""function to multiply 2 matrices"""
-
+"""Matrix multiplication using NumPy."""
 
 import numpy as np
 
 
 def np_matmul(mat1, mat2):
-    """ multiply two matrices
-
-    Args:
-        mat1, mat2: Given matrices
-
-    Return:
-        the new mat: new_mat
-
-    """
-    new_mat = np.dot(mat1, mat2)
-    return new_mat
+    """Return the matrix product of mat1 and mat2."""
+    return np.dot(mat1, mat2)
