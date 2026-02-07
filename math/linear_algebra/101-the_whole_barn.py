bc1c613d19f2ca38335194cbfce644ea94b7ab25
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-02-07 10:28:21 +0400

    Update 101-the_whole_barn.py

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -1,55 +1,25 @@
 #!/usr/bin/env python3
-"""function to add n dimension matrices with the same shape"""
+"""Add two matrices of any dimension."""
 
 
-def shape(matrix):
-    """ return the shape of a matrix
-
-    Args:
-        matrix: Given matrix
-
-    Return:
-        the shape of the matrix: ndim
-
-    """
-    if type(matrix[0]) != list:
-        return [len(matrix)]
-    else:
-        return [len(matrix)] + shape(matrix[0])
+def _shape(matrix):
+    """Return the shape of a (nested-list) matrix."""
+    if not isinstance(matrix, list):
+        return []
+    if matrix and isinstance(matrix[0], list):
+        return [len(matrix)] + _shape(matrix[0])
+    return [len(matrix)]
 
 
-def rec_matrix(mat1, mat2):
-    """ recursively operate an add of a n matrix
-
-        Args:
-            mat1, mat2: Given matrix
-
-        Return:
-            the addition of mat1, mat2 iterating recursively: ndim
-        """
-    new_mat = []
-
-    if (type(mat1) and type(mat2)) == list:
-        for i in range(len(mat1)):
-            if type(mat1[i]) == list:
-                new_mat.append(rec_matrix(mat1[i], mat2[i]))
-            else:
-                new_mat.append(mat1[i] + mat2[i])
-        return new_mat
+def _add(mat1, mat2):
+    """Recursively add two matrices (same shape)."""
+    if isinstance(mat1, list):
+        return [_add(a, b) for a, b in zip(mat1, mat2)]
+    return mat1 + mat2
 
 
 def add_matrices(mat1, mat2):
-    """ add n dimesnional matrices with the same shape
-
-    Args:
-        mat1, mat2: Given matrix
-
-    Return:
-        new_mat: Recursively addition of mat1 and mat2
-
-    """
-    if shape(mat1) != shape(mat2):
+    """Add two matrices; return None if shapes differ."""
+    if _shape(mat1) != _shape(mat2):
         return None
-    else:
-        new_mat = rec_matrix(mat1, mat2)
-        return new_mat
+    return _add(mat1, mat2)
