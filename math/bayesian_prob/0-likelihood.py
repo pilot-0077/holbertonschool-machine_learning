370d56a6bf8f74262c3a325c1693a5c05929ff76
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-05-20 10:06:32 +0400

    Refactor error messages for input validation

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -8,19 +8,27 @@ def likelihood(x, n, P):
     """Calculates the likelihood of obtaining x successes in n trials."""
 
     if not isinstance(n, int) or n <= 0:
-        raise ValueError("n must be a positive integer")
+        raise ValueError(
+            "n must be a positive integer"
+        )
 
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0"
+        )
 
     if x > n:
         raise ValueError("x cannot be greater than n")
 
     if not isinstance(P, np.ndarray) or len(P.shape) != 1:
-        raise TypeError("P must be a 1D numpy.ndarray")
+        raise TypeError(
+            "P must be a 1D numpy.ndarray"
+        )
 
     if np.any(P < 0) or np.any(P > 1):
-        raise ValueError("All values in P must be in the range [0, 1]")
+        raise ValueError(
+            "All values in P must be in the range [0, 1]"
+        )
 
     factorial_n = 1
     factorial_x = 1
