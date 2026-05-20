31f02be5673a455112c7b2264b4c96f42eff0dd0
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-05-20 10:12:29 +0400

    Add files via upload

---
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Module that defines one-hot encoding."""
+
+
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    """Converts a numeric label vector into a one-hot matrix."""
+
+    if not isinstance(Y, np.ndarray) or len(Y.shape) != 1:
+        return None
+
+    if not isinstance(classes, int) or classes <= np.max(Y):
+        return None
+
+    try:
+        one_hot = np.zeros((classes, Y.shape[0]))
+        one_hot[Y, np.arange(Y.shape[0])] = 1
+        return one_hot
+    except Exception:
+        return None
