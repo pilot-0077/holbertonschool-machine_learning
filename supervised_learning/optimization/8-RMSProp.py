cf603abf39704b1698af22820b84475797ee697f
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-07-29 15:31:42 +0400

    Update 8-RMSProp.py

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
-"""Script to optimize DNN using RMSprop with Tensorflow"""
+"""Creates an RMSprop optimizer in TensorFlow."""
 
 import tensorflow as tf
 
 
-def create_RMSProp_op(loss, alpha, beta2, epsilon):
-    """
-    Function to train a DNN with TF RMSProp optimization
+def create_RMSProp_op(alpha, beta2, epsilon):
+    """Creates a TensorFlow Keras RMSprop optimizer.
+
     Args:
-        loss: loss of the network
-        alpha: learning rate
-        beta2: RMSProp weight
-        epsilon: small number to avoid division by zero
+        alpha: Learning rate.
+        beta2: RMSprop decay rate.
+        epsilon: Small constant for numerical stability.
 
-    Returns: momentum optimization operation
-
+    Returns:
+        A TensorFlow Keras RMSprop optimizer.
     """
-    optimizer = tf.train.RMSPropOptimizer(learning_rate=alpha,
-                                          decay=beta2,
-                                          epsilon=epsilon).minimize(loss)
+    optimizer = tf.keras.optimizers.RMSprop(
+        learning_rate=alpha,
+        rho=beta2,
+        epsilon=epsilon
+    )
+
     return optimizer
