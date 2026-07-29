e6909657ee28f1b766cc2c2243f7f2ebec30bb96
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-07-29 15:44:26 +0400

    Update 14-batch_norm.py

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -1,36 +1,38 @@
 #!/usr/bin/env python3
-"""Script to create a batch normalization layer in a DNN
-    using tensorflow"""
+"""Creates a batch normalization layer."""
 
 import tensorflow as tf
 
 
 def create_batch_norm_layer(prev, n, activation):
-    """
-    Function that normalized a batch in a DNN with Tf
+    """Creates a dense layer followed by batch normalization.
+
     Args:
-        prev: the activated output of the previous layer
-        n: number of nodes in the layer to be created
-        activation: activation function that should be used
-                    on the output of the layer
+        prev: Activated output of the previous layer.
+        n: Number of nodes in the new layer.
+        activation: Activation function to apply.
 
-    Returns: tensor of the activated output for the layer
-
+    Returns:
+        The activated output tensor of the new layer.
     """
-    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    x = tf.layers.Dense(units=n, activation=None, kernel_initializer=init)
-    x_prev = x(prev)
-    scale = tf.Variable(tf.constant(1.0, shape=[n]), name='gamma')
-    mean, variance = tf.nn.moments(x_prev, axes=[0])
-    offset = tf.Variable(tf.constant(0.0, shape=[n]), name='beta')
-    variance_epsilon = 1e-8
-
-    normalization = tf.nn.batch_normalization(
-        x_prev,
-        mean,
-        variance,
-        offset,
-        scale,
-        variance_epsilon,
+    initializer = tf.keras.initializers.VarianceScaling(
+        scale=2.0,
+        mode='fan_avg'
     )
-    return activation(normalization)
+
+    dense = tf.keras.layers.Dense(
+        units=n,
+        activation=None,
+        kernel_initializer=initializer
+    )
+
+    output = dense(prev)
+
+    batch_norm = tf.keras.layers.BatchNormalization(
+        axis=-1,
+        epsilon=1e-8
+    )
+
+    normalized = batch_norm(output)
+
+    return activation(normalized)
