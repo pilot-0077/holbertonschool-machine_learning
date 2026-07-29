47c38001782f48451e1f18bdff5f461ac6c7fc97
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-07-29 15:24:26 +0400

    Update 6-momentum.py

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python3
-"""Creates a momentum optimization operation in TensorFlow."""
+"""Creates a Keras optimizer using gradient descent with momentum."""
 
 import tensorflow as tf
 
 
-def create_momentum_op(loss, alpha, beta1):
-    """Creates a momentum optimizer training operation.
+def create_momentum_op(alpha, beta1):
+    """Creates an SGD optimizer with momentum.
 
     Args:
-        loss: Loss tensor of the network.
         alpha: Learning rate.
         beta1: Momentum weight.
 
     Returns:
-        The momentum optimization operation.
+        A TensorFlow Keras SGD optimizer.
     """
-    optimizer = tf.compat.v1.train.MomentumOptimizer(
+    optimizer = tf.keras.optimizers.SGD(
         learning_rate=alpha,
         momentum=beta1
     )
 
-    return optimizer.minimize(loss)
+    return optimizer
