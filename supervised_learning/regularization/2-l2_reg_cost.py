0aa95e8a53af0be5113325c52720d3bc476fb486
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-08-06 10:00:27 +0400

    Modify l2_reg_cost to include model parameter
    Updated the l2_reg_cost function to include the model parameter and calculate regularization losses for each layer.

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
-"""L2 regularization in tensorflow"""
+"""Calculates the cost of a Keras model with L2 regularization."""
 
 import tensorflow as tf
 
 
-def l2_reg_cost(cost):
-    """
-    Function to calculate the L2 regularization in tf
+def l2_reg_cost(cost, model):
+    """Calculates the cost with L2 regularization for each layer.
+
     Args:
-        cost: Tensor containing the cost of the network without
-              L2 regularization
+        cost: Tensor containing the cost without L2 regularization.
+        model: Keras model containing layers with L2 regularization.
 
-    Returns: Tensor containing the cost of the network accounting
-             for L2 regularization
-
+    Returns:
+        Tensor containing the cost for each regularized layer.
     """
+    regularization_losses = tf.stack(model.losses)
 
-    return cost + tf.losses.get_regularization_losses()
+    return cost + regularization_losses
