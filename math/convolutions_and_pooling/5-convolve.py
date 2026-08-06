02e65e1d54350ef504cbed224e8648183a0c0b4e
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-08-06 17:59:50 +0400

    Refactor convolve function for better readability
    Refactor the convolve function for improved clarity and efficiency.

--- math/convolutions_and_pooling/5-convolve.py
+++ math/convolutions_and_pooling/5-convolve.py
@@ -1,71 +1,69 @@
 #!/usr/bin/env python3
-"""Script to perform a convolution of images with multiple kernels"""
+"""Performs a convolution on images using multiple kernels."""
 
 import numpy as np
 
 
 def convolve(images, kernels, padding='same', stride=(1, 1)):
-    """
-    Function to perform a convolution of img with channels
-    Args:
-        images: numpy.ndarray with shape (m, h, w) containing
-                multiple grayscale images
-                m: the number of images
-                h: height in pixels of the images
-                w: width in pixels of the images
-        kernels: numpy.ndarray with shape (kh, kw, c, nc) containing
-                the kernel for the convolution
-                kn: the height of the kernel
-                kw: the width of the kernel
-                c:  the number of channels in the image
-                nc: number of kernels
-        padding: is either a tuple of (ph, pw), 'same, 'valid'
-                 ph: is the padding for the height of the image
-                 pw: is the padding for the width of the image
-        stride: is a tuple of (sh, sw)
-                sh is the stride for the height of the image
-                sw is the stride for the width of the image
-    Returns: numpy.ndarray containing the convolved images
+    """Performs a convolution on images using multiple kernels.
 
+    Args:
+        images: NumPy array with shape (m, h, w, c) containing images.
+        kernels: NumPy array with shape (kh, kw, c, nc).
+        padding: Either 'same', 'valid', or a tuple (ph, pw).
+        stride: Tuple (sh, sw) containing the strides.
+
+    Returns:
+        NumPy array containing the convolved images.
     """
-    m = images.shape[0]
-    h = images.shape[1]
-    w = images.shape[2]
-    c = images.shape[3]
-    kh = kernels.shape[0]
-    kw = kernels.shape[1]
-    nc = kernels.shape[3]  # Needs to be the same frequency of c
-    pad_w = 0
-    pad_h = 0
-    sh = stride[0]
-    sw = stride[1]
+    m, h, w, _ = images.shape
+    kh, kw, _, nc = kernels.shape
+    sh, sw = stride
 
     if padding == 'same':
-        pad_h = int((((h - 1) * sh + kh - h) / 2) + 1)
-        pad_w = int((((w - 1) * sw + kw - w) / 2) + 1)
-    if type(padding) == tuple:
-        pad_h = padding[0]
-        pad_w = padding[1]
+        pad_h = int(np.ceil(
+            ((h - 1) * sh + kh - h) / 2
+        ))
+        pad_w = int(np.ceil(
+            ((w - 1) * sw + kw - w) / 2
+        ))
+    elif padding == 'valid':
+        pad_h = 0
+        pad_w = 0
+    elif isinstance(padding, tuple):
+        pad_h, pad_w = padding
 
-    image_pad = np.pad(images, pad_width=((0, 0), (pad_h, pad_h),
-                                          (pad_w, pad_w), (0, 0)),
-                       mode='constant')
+    images_padded = np.pad(
+        images,
+        (
+            (0, 0),
+            (pad_h, pad_h),
+            (pad_w, pad_w),
+            (0, 0)
+        ),
+        mode='constant'
+    )
 
-    output_h = int(((h + 2 * pad_h - kh) / sh) + 1)
-    output_w = int(((w + 2 * pad_w - kw) / sw) + 1)
+    output_h = ((h + (2 * pad_h) - kh) // sh) + 1
+    output_w = ((w + (2 * pad_w) - kw) // sw) + 1
 
-    # convolution output
-    conv_out = np.zeros((m, output_h, output_w, nc))
+    convolved = np.zeros((m, output_h, output_w, nc))
 
-    image = np.arange(m)
-    # Loop every pixel of the output
-    for x in range(output_h):
-        for y in range(output_w):
-            for z in range(nc):
-                # element wise multiplication of the kernel and the image
-                conv_out[image, x, y, z] = (np.sum(image_pad[image,
-                                                   x * sh:((x * sh) + kh),
-                                                   y * sw:((y * sw) + kw)] *
-                                                   kernels[:, :, :, z],
-                                                   axis=(1, 2, 3)))
-    return conv_out
+    for i in range(output_h):
+        for j in range(output_w):
+            row_start = i * sh
+            col_start = j * sw
+
+            region = images_padded[
+                :,
+                row_start:row_start + kh,
+                col_start:col_start + kw,
+                :
+            ]
+
+            convolved[:, i, j, :] = np.sum(
+                region[:, :, :, :, np.newaxis] * kernels,
+                axis=(1, 2, 3)
+            )
+
+    return convolved
