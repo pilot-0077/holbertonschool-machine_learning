f5bf6c4931410059c951994b7ecd62e849321751
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-08-11 17:14:31 +0400

    Update 4-brightness.py

--- pipeline/data_augmentation/4-brightness.py
+++ pipeline/data_augmentation/4-brightness.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
-""" brightness an image"""
+"""Randomly changes the brightness of an image."""
+
 import tensorflow as tf
 
 
 def change_brightness(image, max_delta):
-    """brightness an image"""
-    img = tf.image.adjust_brightness(image, max_delta)
-    return img
+    """Randomly change the brightness of an image.
+
+    Args:
+        image: A 3D tf.Tensor containing the image.
+        max_delta: Maximum amount to brighten or darken the image.
+
+    Returns:
+        The image with randomly adjusted brightness.
+    """
+    return tf.image.random_brightness(image, max_delta)
