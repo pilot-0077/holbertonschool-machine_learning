1485df5609796adaedd30318dd7cc5a5667df985
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-08-11 16:21:36 +0400

    Update 1-crop.py

--- pipeline/data_augmentation/1-crop.py
+++ pipeline/data_augmentation/1-crop.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
-""" crop_image"""
+"""Performs a random crop of an image."""
+
 import tensorflow as tf
 
 
 def crop_image(image, size):
-    """crop image"""
-    img = tf.random_crop(image, size=size)
-    return img
+    """Perform a random crop of an image.
+
+    Args:
+        image: A 3D tf.Tensor containing the image to crop.
+        size: A tuple containing the size of the crop.
+
+    Returns:
+        The randomly cropped image.
+    """
+    return tf.image.random_crop(image, size=size)
