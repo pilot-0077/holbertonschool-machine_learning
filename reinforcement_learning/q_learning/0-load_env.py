508e0c90b8ae450f60e78720526ea18c69ab34d3
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-08-12 09:24:48 +0400

    Fix FrozenLake loader import restriction

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python3
-"""Module for loading the FrozenLake environment."""
+"""Module that loads the FrozenLake environment."""
 
 import gymnasium as gym
-from gymnasium.envs.toy_text.frozen_lake import generate_random_map
 
 
 def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
-    """Load a FrozenLake environment.
+    """Load and return a FrozenLake environment.
 
     Args:
-        desc: Custom map description.
+        desc: Custom description of the map.
         map_name: Name of a pre-made map.
         is_slippery: Whether the ice is slippery.
 
     Returns:
         The FrozenLake environment.
     """
-    if desc is None and map_name is None:
-        desc = generate_random_map(size=8)
-
     return gym.make(
         "FrozenLake-v1",
         desc=desc,
