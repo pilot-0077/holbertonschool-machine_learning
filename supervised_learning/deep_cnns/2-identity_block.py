732864006586152c698cb75c27d2d83044627143
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-08-13 15:23:23 +0400

    Fix ResNet identity block implementation

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -1,50 +1,48 @@
 #!/usr/bin/env python3
-"""Script to create an inception block"""
+"""Builds an identity block for a deep residual network."""
 
-import tensorflow.keras as K
+from tensorflow import keras as K
 
 
 def identity_block(A_prev, filters):
-    """
+    """Build an identity block.
 
     Args:
-        A_prev: the output from the previous layer
-        filters: tuple or list containing the following filters
-                 F11: the number of filters in the 1st 1x1 convolution
-                 F3: the number of filters in the 3x3 convolution
-                 F12: the number of filters in the 2nd 1x1 convolution
+        A_prev: Output from the previous layer.
+        filters: Tuple or list containing F11, F3, and F12.
 
-    Returns: the activated output of the identity block
+    Returns:
+        The activated output of the identity block.
     """
-    init = K.initializers.he_normal()
-    activation = 'relu'
     F11, F3, F12 = filters
 
-    # First component of the main path
-    conv1 = K.layers.Conv2D(filters=F11, kernel_size=1, padding='same',
-                            kernel_initializer=init)(A_prev)
+    conv1 = K.layers.Conv2D(
+        filters=F11,
+        kernel_size=(1, 1),
+        padding='same',
+        kernel_initializer=K.initializers.he_normal(seed=0)
+    )(A_prev)
+    batch1 = K.layers.BatchNormalization(axis=3)(conv1)
+    relu1 = K.layers.Activation('relu')(batch1)
 
-    batchc1 = K.layers.BatchNormalization(axis=3)(conv1)
+    conv2 = K.layers.Conv2D(
+        filters=F3,
+        kernel_size=(3, 3),
+        padding='same',
+        kernel_initializer=K.initializers.he_normal(seed=0)
+    )(relu1)
+    batch2 = K.layers.BatchNormalization(axis=3)(conv2)
+    relu2 = K.layers.Activation('relu')(batch2)
 
-    relu1 = K.layers.Activation('relu')(batchc1)
-
-    # Second component of the main path
-    conv2 = K.layers.Conv2D(filters=F3, kernel_size=3, padding='same',
-                            kernel_initializer=init)(relu1)
-
-    batchc2 = K.layers.BatchNormalization(axis=3)(conv2)
-
-    relu2 = K.layers.Activation('relu')(batchc2)
-
-    # Third component of the main path
-    conv3 = K.layers.Conv2D(filters=F12, kernel_size=1, padding='same',
-                            kernel_initializer=init)(relu2)
-
+    conv3 = K.layers.Conv2D(
+        filters=F12,
+        kernel_size=(1, 1),
+        padding='same',
+        kernel_initializer=K.initializers.he_normal(seed=0)
+    )(relu2)
     batch3 = K.layers.BatchNormalization(axis=3)(conv3)
 
-    # Add shortcut to main path, pass through a relu activation
-    add = K.layers.Add()([batch3, A_prev])
+    added = K.layers.Add()([batch3, A_prev])
+    output = K.layers.Activation('relu')(added)
 
-    final_relu = K.layers.Activation('relu')(add)
-
-    return final_relu
+    return output
