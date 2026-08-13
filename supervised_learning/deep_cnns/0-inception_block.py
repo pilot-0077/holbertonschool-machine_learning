707ae47dcd9ad3a355ab12aa0cb9f7b15a1d558b
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-08-13 17:25:04 +0400

    Fix Inception block implementation

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -1,58 +1,64 @@
 #!/usr/bin/env python3
-"""Script to create an inception block"""
+"""Builds an Inception block."""
 
-import tensorflow.keras as K
+from tensorflow import keras as K
 
 
 def inception_block(A_prev, filters):
-    """
-    Function to create an inception block
+    """Build an Inception block.
+
     Args:
-        A_prev: The output from the previous layer
-        filters: Tuple or list containing the following filters
-                 F1: is the number of filters in the 1x1 convolution
-                 F3R: is the number of filters in the 1x1 convolution
-                      before the 3x3 convolution
-                 F3: is the number of filters in the 3x3 convolution
-                 F5R: is the number of filters in the 1x1 convolution
-                      before the 5x5 convolution
-                 F5: is the number of filters in the 5x5 convolution
-                 FPP: is the number of filters in the 1x1 convolution
-                      after the max pooling
-    Returns: the concatenated output of the inception block
+        A_prev: Output from the previous layer.
+        filters: Tuple or list containing F1, F3R, F3, F5R, F5, and FPP.
+
+    Returns:
+        The concatenated output of the Inception block.
     """
-    activation = 'relu'
-    init = K.initializers.he_normal(seed=None)
     F1, F3R, F3, F5R, F5, FPP = filters
 
-    convly_1 = K.layers.Conv2D(filters=F1, kernel_size=1, padding='same',
-                               activation=activation,
-                               kernel_initializer=init)(A_prev)
+    conv1 = K.layers.Conv2D(
+        filters=F1,
+        kernel_size=(1, 1),
+        padding='same',
+        activation='relu'
+    )(A_prev)
 
-    convly_2P = K.layers.Conv2D(filters=F3R, kernel_size=1, padding='same',
-                                activation=activation,
-                                kernel_initializer=init)(A_prev)
+    conv3_reduce = K.layers.Conv2D(
+        filters=F3R,
+        kernel_size=(1, 1),
+        padding='same',
+        activation='relu'
+    )(A_prev)
+    conv3 = K.layers.Conv2D(
+        filters=F3,
+        kernel_size=(3, 3),
+        padding='same',
+        activation='relu'
+    )(conv3_reduce)
 
-    convly_2 = K.layers.Conv2D(filters=F3, kernel_size=3, padding='same',
-                               activation=activation,
-                               kernel_initializer=init)(convly_2P)
+    conv5_reduce = K.layers.Conv2D(
+        filters=F5R,
+        kernel_size=(1, 1),
+        padding='same',
+        activation='relu'
+    )(A_prev)
+    conv5 = K.layers.Conv2D(
+        filters=F5,
+        kernel_size=(5, 5),
+        padding='same',
+        activation='relu'
+    )(conv5_reduce)
 
-    convly_3P = K.layers.Conv2D(filters=F5R, kernel_size=1, padding='same',
-                                activation=activation,
-                                kernel_initializer=init)(A_prev)
+    pool = K.layers.MaxPooling2D(
+        pool_size=(3, 3),
+        strides=(1, 1),
+        padding='same'
+    )(A_prev)
+    pool_proj = K.layers.Conv2D(
+        filters=FPP,
+        kernel_size=(1, 1),
+        padding='same',
+        activation='relu'
+    )(pool)
 
-    convly_3 = K.layers.Conv2D(filters=F5, kernel_size=5, padding='same',
-                               activation=activation,
-                               kernel_initializer=init)(convly_3P)
-
-    layer_pool = K.layers.MaxPooling2D(pool_size=[3, 3], strides=(1, 1),
-                                       padding='same')(A_prev)
-
-    layer_poolP = K.layers.Conv2D(filters=FPP, kernel_size=1, padding='same',
-                                  activation=activation,
-                                  kernel_initializer=init)(layer_pool)
-
-    mid_layer = K.layers.concatenate([convly_1, convly_2,
-                                      convly_3, layer_poolP])
-
-    return mid_layer
+    return K.layers.Concatenate()([conv1, conv3, conv5, pool_proj])
